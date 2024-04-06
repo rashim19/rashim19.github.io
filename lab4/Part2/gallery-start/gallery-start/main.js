1f0704974b0a8b403ee175a10cf3f6a50b0093bc
rashim19 <158232107+rashim19@users.noreply.github.com>
2024-04-05 22:56:38 -0400

    images
    adding images

--- lab4/Part2/gallery-start/gallery-start/main.js
+++ lab4/Part2/gallery-start/gallery-start/main.js
@@ -7,6 +7,15 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 
 /* Declaring the alternative text for each image file */
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic1.jpg' : 'a human eye',
+  'pic2.jpg' : 'a wave',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Section of wall',
+  'pic5.jpg' : 'a Large moth'
+}
+
 
 /* Looping through images */
 

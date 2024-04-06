bd6cd78c73504e202b2df3370d26040c655df2a1
rashim19 <158232107+rashim19@users.noreply.github.com>
2024-04-05 22:58:33 -0400

    for loop
    applying for loop through images

--- lab4/Part2/gallery-start/gallery-start/main.js
+++ lab4/Part2/gallery-start/gallery-start/main.js
@@ -18,10 +18,17 @@ const alts = {
 
 
 /* Looping through images */
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+
 
 /* Wiring up the Darken/Lighten button */

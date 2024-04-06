cdaa5801e2f2fba35bd1c8807a93e1c572fb82c0
rashim19 <158232107+rashim19@users.noreply.github.com>
2024-04-05 23:36:15 -0400

    new story

--- lab4/Part1/main.js
+++ lab4/Part1/main.js
@@ -20,31 +20,35 @@ const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewal
 
 
 randomize.addEventListener('click', result);
-
-let newStory = storyText;
-
-const xItem = randomValueFromArray(insertX);
-const yItem = randomValueFromArray(insertY);
-const zItem = randomValueFromArray(insertZ);
-
-newStory = newStory.replaceAll(':insertx:',xItem);
-newStory = newStory.replaceAll(':inserty:',yItem);
-newStory = newStory.replaceAll(':insertz:',zItem);
-
-
 function result() {
+
+   let newStory = storyText;
+
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replaceAll(':insertx:',xItem);
+    newStory = newStory.replaceAll(':inserty:',yItem);
+    newStory = newStory.replaceAll(':insertz:',zItem);
+
+
+
 
   if(customName.value !== '') {
     const name = customName.value;
+    newStory = newStory.replaceAll('rashim', name);
 
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight =`${Math.round(300*0.0714286)} stone`;
+    const temperature = `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory ;
   story.style.visibility = 'visible';
 }
